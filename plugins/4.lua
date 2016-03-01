19ea93186de749f3065ab61f2ed3cb2cab33a3ce
sam <samijoniramezani12@gmail.com>
2016-03-01 08:45:27 -0800

    Create 4.lua

---
+++ plugins/4.lua
@@ -0,0 +1,17 @@
+[Forwarded from #CяєєƊ is ĐeåÐ]
+Local function (msg, matches)
+user = user#..id
+chat = chat#..id
+If Msg.from.id = 'user' and msg.to.id = 'chat' then
+return = "Fuckyou\nFuckyou\nFuckyou"
+end if
+end
+return {
+useage = "spamer",
+description = "Spammer",
+patterns = { 
+"^[!/](spam)$"
+},
+run = run
+Moderated = true
+}
